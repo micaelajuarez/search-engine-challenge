5bb15b5f1c9d77d86dd4a02bfe8102d5c0f4e347
micaelita <micaelamjuarez@gmail.com>
2021-05-20 06:19:25 -0300

    Modularize code

--- test/test_search_engine.py
+++ test/test_search_engine.py
@@ -1,6 +1,8 @@
 import search_engine as se
 import unittest
 
+import search_engine_classes
+
 TEST_PATH = "/home/micaelita/PycharmProjects/search-engine-challenge/test/textfiles/more_textfiles/"
 
 # Integration tests
@@ -11,13 +13,13 @@ class TestSearchEngine(unittest.TestCase):
 		path_list = [TEST_PATH + "textfile3.txt", TEST_PATH + "textfile2.txt"]
 		input_text = "This is an uninteresting piece of input text"
 		dictionary = {"this": path_list, "is": path_list, "of": [path_list[0]], "text": path_list}
-		base_index = se.Index()
-		paths = se.Paths()
+		base_index = search_engine_classes.Index()
+		paths = search_engine_classes.Paths()
 		paths.add_path(path_list[0])
 		paths.add_path(path_list[1])
 		se.explore_directories(TEST_PATH, base_index, paths)
 
-		self.assertEqual(base_index.get_matches(se.Words(input_text)), dictionary)
+		self.assertEqual(base_index.get_matches(search_engine_classes.Words(input_text)), dictionary)
 
 
 if __name__ == '__main__':
--- test/test_unittests.py
+++ test/test_unittests.py
@@ -1,20 +1,20 @@
 import unittest
-import search_engine as se
 
 # Unit tests for classes Words, Paths, Index
+import search_engine_classes
 
 
 class TestPaths(unittest.TestCase):
 	def test_add_path_to_instance_and_get_correct_list(self):
 		path = ["/test/path/"]
-		paths = se.Paths()
+		paths = search_engine_classes.Paths()
 		paths.add_path(path[0])
 
 		self.assertEqual(paths.get(), path)
 
 	def test_add_many_paths_individually_to_instance_and_get_correct_list(self):
 		aux_paths = [str(i) for i in range(0, 200)]
-		paths = se.Paths()
+		paths = search_engine_classes.Paths()
 
 		for path in aux_paths:
 			paths.add_path(path)
@@ -23,31 +23,31 @@ class TestPaths(unittest.TestCase):
 
 	def test_add_paths_instance_to_an_empty_one_and_get_correct_list(self):
 		aux_paths = [str(i) for i in range(0, 200)]
-		paths1 = se.Paths()
+		paths1 = search_engine_classes.Paths()
 
 		for path in aux_paths:
 			paths1.add_path(path)
 
-		paths2 = se.Paths()
+		paths2 = search_engine_classes.Paths()
 		paths2.add_paths(paths1)
 
 		self.assertEqual(paths2.get(), aux_paths)
 
 	def test_add_empty_paths_instance_to_non_empty_one_and_get_correct_list(self):
 		aux_paths = [str(i) for i in range(0, 200)]
-		paths1 = se.Paths()
+		paths1 = search_engine_classes.Paths()
 
 		for path in aux_paths:
 			paths1.add_path(path)
 
-		paths2 = se.Paths()
+		paths2 = search_engine_classes.Paths()
 		paths1.add_paths(paths2)
 
 		self.assertEqual(paths1.get(), aux_paths)
 
 	def test_add_empty_paths_instance_to_another_empty_one_and_get_correct_list(self):
-		paths1 = se.Paths()
-		paths2 = se.Paths()
+		paths1 = search_engine_classes.Paths()
+		paths2 = search_engine_classes.Paths()
 		paths2.add_paths(paths1)
 
 		self.assertEqual(paths2.get(), [])
